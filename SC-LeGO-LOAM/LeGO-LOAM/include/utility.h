6c5a628eded716c9a7c4b037291934d9a2d8c90f
Offliners <eric21904@gmail.com>
2023-03-08 19:45:36 +0800

    update

--- SC-LeGO-LOAM/LeGO-LOAM/include/utility.h
+++ SC-LeGO-LOAM/LeGO-LOAM/include/utility.h
@@ -52,9 +52,9 @@ using namespace std;
 
 typedef pcl::PointXYZI  PointType;
 
-// extern const string pointCloudTopic = "/velodyne_points";
+extern const string pointCloudTopic = "/velodyne_points";
 // extern const string pointCloudTopic = "/kitti_scan";
-extern const string pointCloudTopic = "/os1_points";
+// extern const string pointCloudTopic = "/os1_points";
 extern const string imuTopic = "/imu/data";
 
 // Save pcd
@@ -98,12 +98,20 @@ extern const bool useCloudRing = false; // if true, ang_res_y and ang_bottom are
 // extern const int groundScanInd = 7;
 
 // Ouster OS1-64
-extern const int N_SCAN = 64;
-extern const int Horizon_SCAN = 1024;
-extern const float ang_res_x = 360.0/float(Horizon_SCAN);
-extern const float ang_res_y = 33.2/float(N_SCAN-1);
-extern const float ang_bottom = 16.6+0.1;
-extern const int groundScanInd = 15;
+// extern const int N_SCAN = 64;
+// extern const int Horizon_SCAN = 1024;
+// extern const float ang_res_x = 360.0/float(Horizon_SCAN);
+// extern const float ang_res_y = 33.2/float(N_SCAN-1);
+// extern const float ang_bottom = 16.6+0.1;
+// extern const int groundScanInd = 15;
+
+//Leishen C16
+extern const int N_SCAN = 16;
+extern const int Horizon_SCAN = 2000;
+extern const float ang_res_x = 0.18;
+extern const float ang_res_y = 2.0;
+extern const float ang_bottom = 15.0;
+extern const int groundScanInd = 10;
 
 extern const bool loopClosureEnableFlag = true;
 extern const double mappingProcessInterval = 0.3;
